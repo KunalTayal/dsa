5c05272325568bff1a6e22ec56174a85d02236a1
KunalTayal <kunaltayal97@gmail.com>
2026-08-04 00:05:32 +0530

    refactor: remove redundant count2 variable and logic in sortArrayUsingCounting function

--- practise ques/sort0s1s2s.dart
+++ practise ques/sort0s1s2s.dart
@@ -25,14 +25,11 @@ sortArrayOf012(List<int> nums) {
  sortArrayUsingCounting(List<int> nums) {
   int count0 = 0;
   int count1 = 0;
-  int count2 = 0;
   for (int i = 0; i < nums.length; i++) {
     if (nums[i] == 0) {
       count0++;
     } else if (nums[i] == 1) {
       count1++;
-    } else if (nums[i] == 2) {
-      count2++;
     }
   }
   for (int i = 0; i < count0; i++) {
